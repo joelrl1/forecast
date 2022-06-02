1c6c4a1aeef39df39b9080bbb417d1ca0e233f7f
Joel <joel1.robert@gmail.com>
2022-06-02 17:53:26 +0200

    eventlistener für vorhersagen weltweit

--- main.js
+++ main.js
@@ -113,3 +113,12 @@ async function loadWeather(url) {
     
 };
 loadWeather("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=47.267222&lon=11.392778");
+// auf Klick auf die Karte reagieren
+map.on("click", function(evt) {
+    //console.log(evt);
+
+    let url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`;
+    //console.log(url);
+
+    loadWeather(url);
+});
